edb600935bfdc175832be0e43331dab01cef50b9
Vinod Magham <maghamvinod@gmail.com>
2021-12-02 13:39:35 +0530

    logbox added

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-// import { AppRegistry } from 'react-native';
-// import App from './App';
-// AppRegistry.registerComponent('Logsin', () => App);
-
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
-import { registerRootComponent } from 'expo';
+import {AppRegistry,LogBox} from 'react-native';
 import App from './src/components/App';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+LogBox.ignoreAllLogs(true)
 
 // For requests
 axios.interceptors.request.use(
@@ -25,8 +21,4 @@ axios.interceptors.request.use(
        return Promise.reject(err);
     }
  );
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
 AppRegistry.registerComponent('main', () => App);
