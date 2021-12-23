7b8af4c22995fab74b62315751d136bd36a42792
Vinod Magham <maghamvinod@gmail.com>
2021-12-23 16:51:42 +0530

    intial commit

--- src/Navigation/DrawerContent.js
+++ src/Navigation/DrawerContent.js
@@ -85,6 +85,7 @@ export function DrawerContent(props) {
                             label="Promotions"
                             onPress={() => {props.navigation.navigate('PromoNavigation')}}
                         />
+                        
                         <DrawerItem 
                             icon={({color, size}) => (
                                 <Icon 
