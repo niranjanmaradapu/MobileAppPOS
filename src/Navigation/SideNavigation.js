0393916515e606027e28ea444a4b262ed13ba542
sankar4707 <sankar.thatikonda@otsi.co.in>
2022-05-24 23:30:41 -0700

    sidenav2

--- src/Navigation/SideNavigation.js
+++ src/Navigation/SideNavigation.js
@@ -23,28 +23,28 @@ export default class SideNavigation extends React.Component {
         if (global.previlage1 === 'Dashboard') {
             this.setState({ route: 'Home' })
         } else if (global.previlage2 = 'Billing Portal') {
-
+            this.setState({ route: 'CustomerNavigation' })
         } else if (global.previlage3 = 'Inventory Portal') {
-
+            this.setState({ route: 'InventoryNavigation' })
         } else if (global.previlage4 = 'Promotions & Loyalty') {
-
+            this.setState({ route: 'PromoNavigation' })
         } else if (global.previlage5 = 'Accounting Portal') {
-
+            this.setState({ route: 'AccountingNaviagtion' })
         } else if (global.previlage6 = 'Reports') {
-
+            this.setState({ route: 'ReportsNavigation' })
         } else if (global.previlage7 = 'URM Portal') {
-
+            this.setState({ route: 'UrmNavigation' })
         }
     }
     render() {
         return (
             <Drawer.Navigator
-            initialRouteName={this.state.route}
-            screenOptions={{
-                drawerStyle: {
-                    width: Device.isTablet ? 400 : 300,
-                }  
-            }} drawerContent={props => <DrawerContent {...props} />}>
+                initialRouteName={this.state.route}
+                screenOptions={{
+                    drawerStyle: {
+                        width: Device.isTablet ? 400 : 300,
+                    }
+                }} drawerContent={props => <DrawerContent {...props} />}>
                 <Drawer.Screen name="Home" options={{ headerShown: false }} component={Home} />
                 <Drawer.Screen name="Settings" options={{ headerShown: false }} component={Settings} />
                 <Drawer.Screen name="PromoNavigation" options={{ headerShown: false }} component={PromoNavigation} />
